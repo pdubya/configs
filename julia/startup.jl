2134dd71e61e1988bf6f71717d4a7cb8facadccd
Peter Williams <pdubya@gmail.com>
2023-09-21 22:25:52 -0400

    minor updates

--- julia/startup.jl
+++ julia/startup.jl
@@ -4,9 +4,5 @@ if isfile("Project.toml") && isfile("Manifest.toml")
     Pkg.activate(".")
 end
 
-try
-    using Revise
-catch e
-    @warn "Error initializing Revise" exception=(e, catch_backtrace())
-end
+using Revise
 
