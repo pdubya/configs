68d0fd294713fc03018d39a6cf351e9695b08df6
Peter Williams <pdubya@gmail.com>
2026-02-15 13:23:20 -0500

    minor updates

--- julia/startup.jl
+++ julia/startup.jl
@@ -1,8 +1,11 @@
 
 using Pkg
+if isinteractive()
+    @eval using VimBindings
+end
 if isfile("Project.toml") && isfile("Manifest.toml")
     Pkg.activate(".")
 end
 
 using Revise
-
+using KittyMakie
